9ccc68c941e6358636b43f42d33f659f087b2ce3
Sameer Nadaf <nsameernadaf@gmail.com>
2026-01-18 13:36:12 +0530

    Initial Commit

---
+++ PulseAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PulseAPI
+//
+//  Created by CIPL User01 on 18/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PulseAPI/PulseAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseAPIApp.swift
+//  PulseAPI
+//
+//  Created by CIPL User01 on 18/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PulseAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
